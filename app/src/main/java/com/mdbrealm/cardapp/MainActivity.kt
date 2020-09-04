1761f5a5ce5c0fbd1fe385ad5e46f23497b3b18c
Henna Singh <hennasingh04@gmail.com>
2020-09-05 00:36:11 +0100

    Initial commit

---
+++ app/src/main/java/com/mdbrealm/cardapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mdbrealm.cardapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_constraint)
+    }
+}
